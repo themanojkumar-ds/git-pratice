1ae82c658ec591b1c17c980c21bbb779d56aa3b3
themanojkumar-ds <manojkumar.jobprofile@gmail.com>
2026-03-24 18:40:54 +0530

    added feature to name input

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    name = input("Enter your name:")
+    print(f"Hello {name}!")
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
