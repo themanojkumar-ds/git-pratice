86e79da46b50de109375249eb11716e28c6b6c9d
themanojkumar-ds <manojkumar.jobprofile@gmail.com>
2026-03-25 13:00:08 +0530

    added some new changes

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # some awesome features for feature-1 branch
+    # make some new change in main
     name = input("Enter you name:")
     print_hi(name)
 
